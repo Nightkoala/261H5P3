0789078827ff701227c4cfa1a649a5b69bd4311f
Derek Brown <djb3718@rit.edu>
2014-11-04 14:06:42 -0500

    Added java file to repo

---
+++ CSCI261_hw5_p3/src/MatrixChainParenthesize.java
@@ -0,0 +1,23 @@
+/**
+ * MatrixChainParenthesize.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose:	Find the parenthesizing of a series of matrices that minimizes the
+ * 			number of operations required to multiply them all.
+ */
+
+import java.util.Scanner;
+
+public class MatrixChainParenthesize {
+
+	// Attributes
+	
+	// Constructor
+	
+	// Methods
+	
+	public static void main(String[] args) {
+
+	}// end main
+}//end MatrixChainParenthesize
